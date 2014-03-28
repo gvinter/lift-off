fdda1fdf92bfb9cd2b73b96d26a484464a635a58
Galen Vinter <galen.vinter@gmail.com>
2014-03-28 18:19:17 -0400

    adding to gruntfile.js
    Signed-off-by: Galen Vinter <galen.vinter@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,21 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    // uglify: {
-    //   options: {
-    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-    //   },
-    //   build: {
-    //     src: 'src/<%= pkg.name %>.js',
-    //     dest: 'build/<%= pkg.name %>.min.js'
-    //   }
-    // }
+    uglify: {
+      options: {
+        banner: '/*! main.js <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'js/main.js',
+        dest: 'build/main.min.js'
+      }
+    }
   });
 
   // Load the plugin that provides the "uglify" task.
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Default task(s).
-  // grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify']);
 
 };
